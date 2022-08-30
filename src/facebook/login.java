d1ea157929eb0fc45494e820df6bafebd333fb92
siddu1-34 <siddusiddardha134@gmail.com>
2022-08-30 12:27:31 +0530

    hi google

---
+++ src/facebook/login.java
@@ -0,0 +1,15 @@
+package facebook;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.By;
+
+public class login {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.setProperty("webdriver.chrome.driver","D:\\CHANDRA\\chromedriver_win32 (4)\\chromedriver.exe");
+		WebDriver driver=new ChromeDriver();
+	}
+
+}
